01714458a7dfd2a0d6d40ce91395710c01a6a1e4
H-haruki-20 <hisaharu4019@gmail.com>
2022-02-13 11:32:30 +0900

    JSON形式をPython形式に変換する記述を追加．

--- API.py
+++ API.py
@@ -1,10 +1,11 @@
 import requests
+import json
 
 url = "https://zipcloud.ibsnet.co.jp/api/search"
 payload = {"zipcode":"2440003"}
 
 res = requests.get(url,params=payload)
 print(res.text)
-
-res2 = requests.get(url+"?zipcode=2440003")
-print(res2.text)
+# JSON形式のres.textをpython形式に変換する.
+data = json.loads(res.text)
+print(data)
